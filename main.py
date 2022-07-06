44e0599a0029bb72584aba72a8d4019b47ee21d7
Kattenelvis <officialkattenelvis@gmail.com>
2022-07-06 15:46:38 +0200

    begun from anew kinda

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-
-
 import math as ma
 import numpy as np
 import matplotlib.pyplot as plt 
@@ -31,92 +29,99 @@ class Configuration_path:
 
 
 #TODO put into the class, enter starting configuration if it is used
-configuration_path = Configuration_path(config_length=5, in_sum=5, path_length=5, start_configuration=3)
+configuration_path = Configuration_path(config_length=3, in_sum=3, path_length=5, start_configuration=3)
 lst = [[configuration_path.in_sum-sum(p)] + p for p in configuration_path.partition(configuration_path.in_sum, configuration_path.config_length-1)]
 lst_indx = np.arange(len(lst))
+print(lst)
+print(choice(lst,p=[0.7,0.2,0.1], size=100))
+
+#Likhetsmått: |v1 - v2|/2. Hög likhet -> Högre sannolikhet
+#Entropimått: [3,0,0] = Hög entropi, [1,1,1] = Låg entropi. Min[Sum[Ceil[v1/edges]], Sum[Ceil[v2/edges]]] Hög entropi -> Högre sannolikhet
 
 
-runs = 100
-visited_data = []
-for z in range(runs):
-    similarity = [] 
-    for i in range(len(lst)):
-        similarity_local = []
-        for j in range(len(lst)):
-            if (i == j):
-                similarity_local.append(0.0)
-            else:
-                numerator = np.dot(lst[i], lst[j])
-                proto_denom1 = 0
-                proto_denom2 = 0
-                for k in range(configuration_path.config_length):
-                    proto_denom1 = proto_denom1 + lst[i][k]**2
-                    proto_denom2 = proto_denom2 + lst[j][k]**2
-                denominator = (ma.sqrt(proto_denom1) * ma.sqrt(proto_denom2))
-                similarity_local.append(numerator/denominator)
-        similarity.append(similarity_local)
 
 
-    for i in range(len(lst)):
-        difference_to_one = 1
-        counter = 0
-        summation = 0
-        for j in range(len(lst)):
-            summation = summation + similarity[i][j]
-        for j in range(len(lst)):
-            similarity[i][j] = (similarity[i][j]/summation)
-            similarity[i][j] = round(similarity[i][j], 4)
-            difference_to_one = difference_to_one - similarity[i][j]
-            if (similarity[i][j] > 0):
-                counter = counter + 1 
-        for j in range(len(lst)):
-            if (similarity[i][j] > 0):
-                similarity[i][j] = similarity[i][j] + (difference_to_one/counter)
-
-    #Set starting value
-    for i in range(len(lst)):
-        similarity[i][configuration_path.start_configuration] = 0.0
-    probability_dist = similarity[configuration_path.start_configuration]
-
-    #Random starting value
-    #starting_con = choice(lst_indx, 1)
-    #for i in range(len(lst)):
-    #    similarity[i][starting_con[0]] = 0.0
-    #probability_dist = similarity[starting_con[0]]
+# runs = 100
+# visited_data = []
+# for z in range(runs):
+#     similarity = [] 
+#     for i in range(len(lst)):
+#         similarity_local = []
+#         for j in range(len(lst)):
+#             if (i == j):
+#                 similarity_local.append(0.0)
+#             else:
+#                 numerator = np.dot(lst[i], lst[j])
+#                 proto_denom1 = 0
+#                 proto_denom2 = 0
+#                 for k in range(configuration_path.config_length):
+#                     proto_denom1 = proto_denom1 + lst[i][k]**2
+#                     proto_denom2 = proto_denom2 + lst[j][k]**2
+#                 denominator = (ma.sqrt(proto_denom1) * ma.sqrt(proto_denom2))
+#                 similarity_local.append(numerator/denominator)
+#         similarity.append(similarity_local)
 
 
-    path = []
-    path_indx = []
-    for i in range(0,configuration_path.path_length):
+#     for i in range(len(lst)):
+#         difference_to_one = 1
+#         counter = 0
+#         summation = 0
+#         for j in range(len(lst)):
+#             summation = summation + similarity[i][j]
+#         for j in range(len(lst)):
+#             similarity[i][j] = (similarity[i][j]/summation)
+#             similarity[i][j] = round(similarity[i][j], 4)
+#             difference_to_one = difference_to_one - similarity[i][j]
+#             if (similarity[i][j] > 0):
+#                 counter = counter + 1 
+#         for j in range(len(lst)):
+#             if (similarity[i][j] > 0):
+#                 similarity[i][j] = similarity[i][j] + (difference_to_one/counter)
+
+#     #Set starting value
+#     for i in range(len(lst)):
+#         similarity[i][configuration_path.start_configuration] = 0.0
+#     probability_dist = similarity[configuration_path.start_configuration]
+
+#     #Random starting value
+#     #starting_con = choice(lst_indx, 1)
+#     #for i in range(len(lst)):
+#     #    similarity[i][starting_con[0]] = 0.0
+#     #probability_dist = similarity[starting_con[0]]
+
+
+#     path = []
+#     path_indx = []
+#     for i in range(0,configuration_path.path_length):
         
-        draw = choice(lst_indx, 1, p=probability_dist)
-        path.append(lst[draw[0]])
-        path_indx.append(draw[0])
+#         draw = choice(lst_indx, 1, p=probability_dist)
+#         path.append(lst[draw[0]])
+#         path_indx.append(draw[0])
         
-        for k in range(len(lst)):
-            similarity[k][draw[0]] = 0.0
-            temp_counter = 0
-            temp_difference_to_one = 1
-            for j in range(len(lst)):
-                if (similarity[k][j] > 0):
-                    temp_counter = temp_counter + 1
-                    temp_difference_to_one = temp_difference_to_one - similarity[k][j]
-            for j in range(len(lst)):
-                if (similarity[k][j] > 0):
-                    similarity[k][j] = similarity[k][j] + (temp_difference_to_one/temp_counter)
+#         for k in range(len(lst)):
+#             similarity[k][draw[0]] = 0.0
+#             temp_counter = 0
+#             temp_difference_to_one = 1
+#             for j in range(len(lst)):
+#                 if (similarity[k][j] > 0):
+#                     temp_counter = temp_counter + 1
+#                     temp_difference_to_one = temp_difference_to_one - similarity[k][j]
+#             for j in range(len(lst)):
+#                 if (similarity[k][j] > 0):
+#                     similarity[k][j] = similarity[k][j] + (temp_difference_to_one/temp_counter)
         
-        probability_dist = similarity[draw[0]]
+#         probability_dist = similarity[draw[0]]
     
-    visited_data.append(path_indx)
+#     visited_data.append(path_indx)
     
 
-flat_list = [x for xs in visited_data for x in xs]
-configuration_amount = []
-positionary_vector = []
-for i in range(len(lst)):
-    configuration_amount.append(flat_list.count(i))
-    positionary_vector.append(i)
+# flat_list = [x for xs in visited_data for x in xs]
+# configuration_amount = []
+# positionary_vector = []
+# for i in range(len(lst)):
+#     configuration_amount.append(flat_list.count(i))
+#     positionary_vector.append(i)
 
 
-plt.plot(positionary_vector, configuration_amount)
-plt.show()
+# plt.plot(positionary_vector, configuration_amount)
+# plt.show()
